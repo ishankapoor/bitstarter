dd3c255765e6b58778819f396de0641f9dc92b17
Ishan Kapoor <ishankapoor@gmail.com>
2013-07-06 14:27:11 +0000

    Hellow World from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var indexHTMLBuffer = fs.readFileSync('index.html');
+var indexHTMLString = indexHTMLBuffer.toString('utf-8',0,indexHTMLBuffer.length);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(indexHTMLString);
 });
 
 var port = process.env.PORT || 5000;
